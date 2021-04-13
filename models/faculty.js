7698dbb09022572e418d8a5a482b33d2c534af18
minhpham8502 <minhpqgch18572@fpt.edu.vn>
2021-04-13 22:16:41 +0700

    a

--- models/faculty.js
+++ models/faculty.js
@@ -13,7 +13,7 @@ mongoose.set('useCreateIndex', true);
 const Schema = mongoose.Schema;
 const FacultySchema = new Schema({ 
     facultyname : String,
-    classID: String,
+    facultyID: String,
     deadline:String,
     deadline2:String,
   
